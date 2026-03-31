63697b377b208abd1d59b862b03f7f1e49633b0e
Alternate001 <artem.kushko@publizer.de>
2026-03-31 14:25:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const listOrigin = document.querySelector('ul');
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort((a, b) => {
+    return Number(b.dataset.salary) - Number(a.dataset.salary);
+  });
+  list.append(...items);
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  return items.map((item) => {
+    return {
+      name: item.dataset.name,
+      position: item.dataset.position,
+      salary: Number(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+sortList(listOrigin);
+getEmployees(listOrigin);
